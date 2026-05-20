6464770d16f3fadec42ac6876c3390761c39fd58
HScode1 <88436496+HScode1@users.noreply.github.com>
2026-05-20 15:21:16 +0200

    fixed footer

--- src/imports/HomePage/components/Footer.tsx
+++ src/imports/HomePage/components/Footer.tsx
@@ -1,262 +1,152 @@
 import svgPaths from "../svg-gblhduksgt";
 
-function Svg20() {
+function PhoneIcon() {
   return (
-    <div className="-translate-y-1/2 absolute left-[16px] size-[16px] top-1/2" data-name="SVG">
-      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 16 16">
-        <g id="SVG">
-          <path d={svgPaths.p2a44c680} id="Vector" stroke="var(--stroke-0, black)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.33333" />
-        </g>
-      </svg>
-    </div>
+    <svg className="size-[16px] shrink-0" fill="none" preserveAspectRatio="none" viewBox="0 0 16 16">
+      <g id="SVG">
+        <path d={svgPaths.p2a44c680} id="Vector" stroke="black" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.33333" />
+      </g>
+    </svg>
   );
 }
 
-function FooterLink() {
+function EmailIcon() {
   return (
-    <a className="-translate-x-1/2 absolute bg-[#c8ec66] h-[40px] left-[calc(50%-341.34px)] rounded-[8px] top-[52px] w-[162.76px]" data-name="Link" href="tel:+33670760719">
-      <Svg20 />
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Helvetica_Neue:Regular',sans-serif] h-[18.5px] justify-center leading-[0] left-[calc(50%+12.18px)] not-italic text-[16px] text-black text-center top-[calc(50%-0.25px)] w-[107.125px]">
-        <p className="leading-[24px]">06 70 76 07 19</p>
-      </div>
-    </a>
+    <svg className="size-[16px] shrink-0" fill="none" preserveAspectRatio="none" viewBox="0 0 16 16">
+      <g id="SVG">
+        <path d={svgPaths.p17070980} id="Vector" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.33333" />
+        <path d={svgPaths.p120c8200} id="Vector_2" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.33333" />
+      </g>
+    </svg>
   );
 }
 
-function Svg21() {
+function AddressIcon() {
   return (
-    <div className="-translate-x-1/2 -translate-y-1/2 absolute left-[calc(50%-84.63px)] size-[16px] top-1/2" data-name="SVG">
-      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 16 16">
-        <g id="SVG">
-          <path d={svgPaths.p17070980} id="Vector" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.33333" />
-          <path d={svgPaths.p120c8200} id="Vector_2" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.33333" />
-        </g>
-      </svg>
-    </div>
+    <svg className="size-[16px] shrink-0 mt-1" fill="none" preserveAspectRatio="none" viewBox="0 0 14.15 16.0018">
+      <g id="SVG">
+        <path d={svgPaths.p274fd670} id="Vector" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.17917" />
+        <path d={svgPaths.p1c743f00} id="Vector_2" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.17917" />
+      </g>
+    </svg>
   );
 }
 
-function FooterLink1() {
+function FacebookIcon() {
   return (
-    <a className="absolute h-[24px] left-[16px] right-[698.67px] top-[116px]" data-name="Link" href="mailto:contact@vroomparis.fr">
-      <Svg21 />
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Helvetica_Neue:Regular',sans-serif] h-[18.5px] justify-center leading-[0] left-[calc(50%+12.16px)] not-italic text-[16px] text-center text-white top-[calc(50%-0.25px)] w-[161.595px]">
-        <p className="leading-[24px]">contact@vroomparis.fr</p>
-      </div>
-    </a>
+    <svg className="size-[20px]" fill="none" preserveAspectRatio="none" viewBox="0 0 20 20">
+      <g id="SVG">
+        <path d={svgPaths.p30c8d680} id="Vector" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.66667" />
+      </g>
+    </svg>
   );
 }
 
-function Svg22() {
+function InstagramIcon() {
   return (
-    <div className="-translate-x-1/2 -translate-y-1/2 absolute h-[16.002px] left-[calc(50%-465.93px)] top-[calc(50%+4.5px)] w-[14.15px]" data-name="SVG">
-      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 14.15 16.0018">
-        <g id="SVG">
-          <path d={svgPaths.p274fd670} id="Vector" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.17917" />
-          <path d={svgPaths.p1c743f00} id="Vector_2" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.17917" />
-        </g>
-      </svg>
-    </div>
+    <svg className="size-[20px]" fill="none" preserveAspectRatio="none" viewBox="0 0 20 20">
+      <g id="SVG">
+        <path d={svgPaths.p4b98700} id="Vector" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.66667" />
+        <path d={svgPaths.p19f4a800} id="Vector_2" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.66667" />
+        <path d="M14.5833 5.41667H14.5917" id="Vector_3" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.66667" />
+      </g>
+    </svg>
   );
 }
 
-function FooterLink2() {
+function TwitterIcon() {
   return (
-    <a className="absolute h-[24px] left-0 right-0 top-0" data-name="Link" href="/showroom">
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Helvetica_Neue:Regular',sans-serif] h-[18.5px] justify-center leading-[0] left-[calc(50%+0.19px)] not-italic text-[16px] text-center text-white top-[11.75px] w-[127.557px]">
-        <p className="leading-[24px]">Showroom</p>
-      </div>
-    </a>
-  );
-}
-
-function FooterLink3() {
-  return (
-    <a className="absolute h-[24px] left-0 right-0 top-[32px]" data-name="Link" href="/acheter-votre-vehicule">
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Helvetica_Neue:Regular',sans-serif] h-[19px] justify-center leading-[0] left-1/2 not-italic text-[16px] text-center text-white top-[11.5px] w-[150px]">
-        <p className="leading-[24px]">Acheter un véhicule</p>
-      </div>
-    </a>
-  );
-}
-
-function FooterLink4() {
-  return (
-    <a className="absolute h-[24px] left-0 right-0 top-[68px]" data-name="Link" href="/vendre-votre-vehicule">
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Helvetica_Neue:Regular',sans-serif] h-[19px] justify-center leading-[0] left-1/2 not-italic text-[16px] text-center text-white top-[6.5px] w-[170px]">
-        <p className="leading-[24px]">Vendre votre véhicule</p>
-      </div>
-    </a>
-  );
-}
-
-function FooterLink5() {
-  return <div className="absolute h-[24px] left-0 right-0 top-[96px]" data-name="Link" />;
-}
-
-function Nav() {
-  return (
-    <div className="absolute h-[120px] left-[357.33px] right-[357.33px] top-[48px]" data-name="Nav">
-      <FooterLink2 />
-      <FooterLink3 />
-      <FooterLink4 />
-      <FooterLink5 />
-    </div>
-  );
-}
-
-function FooterLink6() {
-  return (
-    <a className="absolute h-[24px] left-[357.33px] right-[357.33px] top-[144px]" data-name="Link" href="/conseils">
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Helvetica_Neue:Regular',sans-serif] h-[19px] justify-center leading-[0] left-1/2 not-italic text-[16px] text-center text-white top-[11.5px] w-[180px]">
-        <p className="leading-[24px]">Consulation automobile</p>
-      </div>
-    </a>
-  );
-}
-
-function FooterLink7() {
-  return (
-    <a className="absolute h-[24px] left-[357.33px] right-[357.33px] top-[180px]" data-name="Link" href="/a-propos">
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Helvetica_Neue:Regular',sans-serif] h-[19px] justify-center leading-[0] left-1/2 not-italic text-[16px] text-center text-white top-[6.5px] w-[170px]">
-        <p className="leading-[24px]">À propos</p>
-      </div>
-    </a>
-  );
-}
-
-function FooterLink8() {
-  return (
-    <div className="absolute h-[24px] left-[0.34px] right-[-0.34px] top-[-4px]" data-name="Link">
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Helvetica_Neue:Regular',sans-serif] h-[18.5px] justify-center leading-[0] left-[calc(50%+0.17px)] not-italic text-[16px] text-center text-white top-[11.75px] w-[189.407px]">
-        <p className="leading-[24px]">Politique de confidentialité</p>
-      </div>
-    </div>
-  );
-}
-
-function FooterLink9() {
-  return (
-    <div className="absolute h-[24px] left-[0.34px] right-[-0.34px] top-[28px]" data-name="Link">
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Helvetica_Neue:Regular',sans-serif] h-[18.5px] justify-center leading-[0] left-[calc(50%+0.16px)] not-italic text-[16px] text-center text-white top-[11.75px] w-[151.447px]">
-        <p className="leading-[24px]">Conditions générales</p>
-      </div>
-    </div>
-  );
-}
-
-function Nav1() {
-  return (
-    <div className="absolute h-[88px] left-[698.66px] right-[16px] top-[52px]" data-name="Nav">
-      <FooterLink8 />
-      <FooterLink9 />
-    </div>
-  );
-}
-
-function Svg23() {
-  return (
-    <div className="absolute left-0 size-[20px] top-0" data-name="SVG">
-      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 20 20">
-        <g id="SVG">
-          <path d={svgPaths.p30c8d680} id="Vector" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.66667" />
-        </g>
-      </svg>
-    </div>
-  );
-}
-
-function FooterLink10() {
-  return (
-    <div className="-translate-x-1/2 absolute bottom-[93px] left-[calc(50%+306px)] top-[204px] w-[20px]" data-name="Link">
-      <Svg23 />
-    </div>
-  );
-}
-
-function Svg24() {
-  return (
-    <div className="absolute left-0 size-[20px] top-0" data-name="SVG">
-      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 20 20">
-        <g id="SVG">
-          <path d={svgPaths.p4b98700} id="Vector" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.66667" />
-          <path d={svgPaths.p19f4a800} id="Vector_2" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.66667" />
-          <path d="M14.5833 5.41667H14.5917" id="Vector_3" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.66667" />
-        </g>
-      </svg>
-    </div>
-  );
-}
-
-function FooterLink11() {
-  return (
-    <div className="-translate-x-1/2 absolute bottom-[93px] left-[calc(50%+342px)] top-[204px] w-[20px]" data-name="Link">
-      <Svg24 />
-    </div>
-  );
-}
-
-function Svg25() {
-  return (
-    <div className="absolute left-0 size-[20px] top-0" data-name="SVG">
-      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 20 20">
-        <g id="SVG">
-          <path d={svgPaths.p2ffa5d80} id="Vector" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.66667" />
-        </g>
-      </svg>
-    </div>
-  );
-}
-
-function FooterLink12() {
-  return (
-    <div className="-translate-x-1/2 absolute bottom-[93px] left-[calc(50%+378px)] top-[204px] w-[20px]" data-name="Link">
-      <Svg25 />
-    </div>
-  );
-}
-
-function HorizontalBorder() {
-  return (
-    <div className="absolute border-[#1f2937] border-solid border-t h-[49px] left-[16px] right-[16px] top-[268px]" data-name="HorizontalBorder">
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Helvetica_Neue:Regular',sans-serif] h-[18.5px] justify-center leading-[0] left-[calc(50%+0.16px)] not-italic text-[#9ca3af] text-[16px] text-center top-[35.75px] w-[300.597px]">
-        <p className="leading-[24px]">© 2026 Vroom Paris. Tous droits réservés.</p>
-      </div>
-    </div>
-  );
-}
-
-function Container1() {
-  return (
-    <div className="absolute h-[317px] left-[208px] right-[208px] top-[48px]" data-name="Container">
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Wix_Madefor_Display:Regular',sans-serif] font-normal h-[28px] justify-center leading-[0] left-[calc(50%-341.1px)] text-[20px] text-center text-white top-[14px] tracking-[-0.4px] w-[175.161px]">
-        <p className="leading-[28px]">Qu'attendez-vous ?</p>
-      </div>
-      <FooterLink />
-      <FooterLink1 />
-      <Svg22 />
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Helvetica_Neue:Regular',sans-serif] h-[42.5px] justify-center leading-[0] left-[calc(50%-330.17px)] not-italic text-[16px] text-center text-white top-[175.75px] w-[284.77px]">
-        <p className="leading-[24px]">4 bis Av. Alexandre Dumas, 95230 Soisy-sous-Montmorency</p>
-      </div>
-      <Nav />
-      <FooterLink6 />
-      <FooterLink7 />
-      <div className="-translate-x-1/2 -translate-y-1/2 absolute flex flex-col font-['Wix_Madefor_Display:Regular',sans-serif] font-normal h-[28px] justify-center leading-[0] left-[calc(50%+341.55px)] text-[20px] text-center text-white top-[14px] tracking-[-0.4px] w-[152.536px]">
-        <p className="leading-[28px]">Mentions légales</p>
-      </div>
-      <Nav1 />
-      <FooterLink10 />
-      <FooterLink11 />
-      <FooterLink12 />
-      <HorizontalBorder />
-    </div>
+    <svg className="size-[20px]" fill="none" preserveAspectRatio="none" viewBox="0 0 20 20">
+      <g id="SVG">
+        <path d={svgPaths.p2ffa5d80} id="Vector" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.66667" />
+      </g>
+    </svg>
   );
 }
 
 export default function Footer() {
   return (
-    <div className="absolute bg-[#181818] h-[365px] left-0 right-0 top-[7083px]" data-name="Footer">
-      <Container1 />
+    <div className="absolute bg-[#181818] left-0 right-0 top-[7083px]" data-name="Footer">
+      <div className="max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8 pt-12">
+        <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
+
+          {/* Colonne gauche — Contact */}
+          <div className="flex flex-col gap-4">
+            <h3 className="font-['Wix_Madefor_Display:Regular',sans-serif] font-normal text-[20px] text-white tracking-[-0.4px] leading-[28px]">
+              Qu'attendez-vous ?
+            </h3>
+            <a
+              href="tel:+33670760719"
+              className="inline-flex items-center gap-2 bg-[#c8ec66] rounded-[8px] px-4 py-2 w-fit"
+            >
+              <PhoneIcon />
+              <span className="font-['Helvetica_Neue:Regular',sans-serif] text-[16px] text-black leading-[24px]">
+                06 70 76 07 19
+              </span>
+            </a>
+            <a href="mailto:contact@vroomparis.fr" className="flex items-center gap-2">
+              <EmailIcon />
+              <span className="font-['Helvetica_Neue:Regular',sans-serif] text-[16px] text-white leading-[24px]">
+                contact@vroomparis.fr
+              </span>
+            </a>
+            <div className="flex items-start gap-2">
+              <AddressIcon />
+              <span className="font-['Helvetica_Neue:Regular',sans-serif] text-[16px] text-white leading-[24px]">
+                4 bis Av. Alexandre Dumas, 95230 Soisy-sous-Montmorency
+              </span>
+            </div>
+            <div className="flex items-center gap-4 mt-2">
+              <a href="#" aria-label="Facebook"><FacebookIcon /></a>
+              <a href="#" aria-label="Instagram"><InstagramIcon /></a>
+              <a href="#" aria-label="Twitter"><TwitterIcon /></a>
+            </div>
+          </div>
+
+          {/* Colonne centre — Navigation */}
+          <div className="flex flex-col gap-2">
+            <h3 className="font-['Wix_Madefor_Display:Regular',sans-serif] font-normal text-[20px] text-white tracking-[-0.4px] leading-[28px] mb-2">
+              Navigation
+            </h3>
+            <a href="/showroom" className="font-['Helvetica_Neue:Regular',sans-serif] text-[16px] text-white leading-[24px] hover:text-[#c8ec66] transition-colors">
+              Showroom
+            </a>
+            <a href="/acheter-votre-vehicule" className="font-['Helvetica_Neue:Regular',sans-serif] text-[16px] text-white leading-[24px] hover:text-[#c8ec66] transition-colors">
+              Acheter un véhicule
+            </a>
+            <a href="/vendre-votre-vehicule" className="font-['Helvetica_Neue:Regular',sans-serif] text-[16px] text-white leading-[24px] hover:text-[#c8ec66] transition-colors">
+              Vendre votre véhicule
+            </a>
+            <a href="/conseils" className="font-['Helvetica_Neue:Regular',sans-serif] text-[16px] text-white leading-[24px] hover:text-[#c8ec66] transition-colors">
+              Consultation automobile
+            </a>
+            <a href="/a-propos" className="font-['Helvetica_Neue:Regular',sans-serif] text-[16px] text-white leading-[24px] hover:text-[#c8ec66] transition-colors">
+              À propos
+            </a>
+          </div>
+
+          {/* Colonne droite — Mentions légales */}
+          <div className="flex flex-col gap-2">
+            <h3 className="font-['Wix_Madefor_Display:Regular',sans-serif] font-normal text-[20px] text-white tracking-[-0.4px] leading-[28px] mb-2">
+              Mentions légales
+            </h3>
+            <span className="font-['Helvetica_Neue:Regular',sans-serif] text-[16px] text-white leading-[24px] cursor-pointer hover:text-[#c8ec66] transition-colors">
+              Politique de confidentialité
+            </span>
+            <span className="font-['Helvetica_Neue:Regular',sans-serif] text-[16px] text-white leading-[24px] cursor-pointer hover:text-[#c8ec66] transition-colors">
+              Conditions générales
+            </span>
+          </div>
+
+        </div>
+
+        {/* Barre de copyright */}
+        <div className="mt-10 border-t border-[#1f2937] py-6">
+          <p className="font-['Helvetica_Neue:Regular',sans-serif] text-[16px] text-[#9ca3af] text-center leading-[24px]">
+            © 2026 Vroom Paris. Tous droits réservés.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
